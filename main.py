22620e6a713f98f21fcd5794ac3c92f0f60db3ed
sadidshaikh <shaikhsadid431@gmail.com>
2024-08-26 12:58:41 +0530

    Set up project

---
+++ main.py
@@ -0,0 +1,18 @@
+def logger(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with {args} and {kwargs}")
+        return func(*args, **kwargs)
+
+    return wrapper
+
+@logger
+def add(a, b):
+    return a + b
+
+@logger
+def multiply(a, b):
+    return a * b
+
+
+print(add(100, 223))
+print(multiply(23, 4))
